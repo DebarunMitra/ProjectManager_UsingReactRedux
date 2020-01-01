5a4abe42ff5851da7567a16741b932481a01f365
DebarunMitra <debarun96mitra@gmail.com>
2020-01-01 16:25:20 +0530

    components and navbar added

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import {Link} from 'react-router-dom';
+
+const Navbar=()=>{
+    return(
+         <nav className="nav-wrapper grey darken-3">
+             <div className="container">
+                 <Link to='/' className="brand-logo">ProjecPlaner</Link>
+             </div>        
+    </nav>
+    );
+}
+
+export default Navbar;
