e92e71383fbfed2e51a570c089b481abd2259725
Debarun Mitra <debarun96mitra@gmail.com>
2021-09-09 21:45:54 +0530

    preparing react application

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import CandyContainer from './components/layout/CandyContainer';
 
 class App extends Component {
   render(){
@@ -8,6 +9,7 @@ class App extends Component {
       <BrowserRouter>
          <div>
             <Navbar />
+            <CandyContainer />
          </div>
       </BrowserRouter>
    );
---
+++ client/src/components/layout/CandyContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CandyContainer = () => ({
+  render(){
+    return (
+      <div>
+        <h6>Number Of Candy</h6>
+        <button>Buy</button>
+      </div>
+    );
+  }
+});
+
+export default CandyContainer;
